602de4c3aa9146015681a7c2dead5ffb440ff205
vinaygarg <vinaymatt@outlook.com>
2017-05-31 18:53:24 +0530

    Hogan engine integrated succesfully

--- ChitChat/app.js
+++ ChitChat/app.js
@@ -1,8 +1,21 @@
 var express = require('express'),
-app = express();
+	app = express(),
+	path = require('path')
 
+//sets up key value pairs where key = views folder, path = current directory
+app.set('views',path.join(__dirname,'views'));
+
+//To use hogan template engine to render the files
+app.engine('html',require('hogan-express'));
+app.set('view engine','html');
+
+//To find static asset files, we need express static module
+app.use(express.static(path.join(__dirname,'public')));
+
+//route helps to map URL to responses
+// next parameter ensure that it isn't the only route and will look for other routes too.
 app.route('/').get(function(req,res,next) {
-	res.send('Hello World');
+	res.render('index',{'Welcome to ChitChat'});
 })
 
 app.listen(3000,function(){
