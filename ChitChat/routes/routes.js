bbc1fd169092369adc3ef59fcb3e2de9237eff11
vinaygarg <vinaymatt@outlook.com>
2017-07-12 12:59:06 +0530

    all routes done

--- ChitChat/routes/routes.js
+++ ChitChat/routes/routes.js
@@ -1,16 +1,88 @@
 //route helps to map URL to responses
 // next parameter ensure that it isn't the only route and will look for other routes too.
-module.exports = function(express, app){
+module.exports = function(express, app, passport, config, rooms){
+  var router = express.Router();
 
-	var router = express.Router();
-	router.get('/', function(req,res,next){
-		res.render('index',{title : 'Welcome to ChitChat'});
-	})
+  router.get('/', function(req, res, next){
+    res.render('index', {title: 'Welcome to ChatCAT'});
+  });
 
-	router.get('/chatrooms', function(req,res,next){
-		res.render('chatrooms',{title : 'Chatrooms'});
-	})
+  /*
+  * Middleware to check authenticated route.
+  */
+  function securePages(req, res, next){
+    if(req.isAuthenticated()){
+      next();
+    } else {
+      res.redirect('/');
+    }
+  }
 
-	//set default route to be handled by express router instance
-	app.use('/',router);
+  router.get('/auth/facebook', passport.authenticate('facebook'));
+  router.get('/auth/facebook/callback', passport.authenticate('facebook', {
+    successRedirect:'/chatrooms',
+    failureRedirect:'/'
+  }));
+
+  router.get('/logout', function(req, res, next){
+    req.logout();
+    res.redirect('/');
+  });
+
+  router.get('/roomlist', securePages);
+
+  router.get('/chatrooms', securePages, function(req, res, next){
+    res.render('chatrooms', {
+      title: 'Chatrooms',
+      user:req.user,
+      config:config
+    });
+  });
+
+  router.get('/room/:id', securePages, function(req, res, next){
+    var room_name = findTitle(req.params.id); // using req params
+    res.render('room', {
+      title: 'Chatroom',
+      user:req.user,
+      room_number:req.params.id,
+      room_name:room_name,
+      config:config
+    });
+  });
+
+  /*
+  * Search over the rooms list to find the room name.
+  */
+  function findTitle(room_id){
+    var n = 0;
+    while(n < rooms.length){
+      if(rooms[n].room_number == room_id){
+        return rooms[n].room_name;
+        break;
+      } else {
+        n++;
+        continue;
+      }
+    }
+  }
+
+
+
+
+
+
+/*
+* Toy routes
+*/
+  router.get('/setcolor', function(req, res, next){
+    req.session.favColor = "Red";
+    res.send("Setting fave color");
+  })
+
+  router.get('/getcolor', function(req, res, next){
+    res.send("Fav color is set to " + (req.session.favColor===undefined?"Not Found":req.session.favColor));
+  })
+
+  app.use('/', router);
+
 }
